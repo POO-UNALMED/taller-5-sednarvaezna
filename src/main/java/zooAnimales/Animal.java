3819053859046b6b7ee26286a630a02cd8f6e30e
sednarvaezna <sednarvaezna@unal.edu.co>
2020-09-28 02:36:31 -0500

    hola

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -62,7 +62,7 @@ public class Animal {
 		return("desplazarse");
 	}
 	public String toString() {
-		if (this.zona) {
+		if (this.zona != null) {
 			return("Mi nombre es " + this.nombre+ ", tengo una edad de " + this.edad + ", habito en " +  this.habitat + " y mi genero es "+ this.genero + ", la zona en la que me ubico es " + this.zona.getNombre() + ", en el " + this.zona.getZoo().getNombre());
 		}
 		else {
